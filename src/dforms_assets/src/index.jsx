fff5d4980b1c9a68b1ca714bf25a5a0f4eae1851
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-06-20 15:57:43 +0530

    Added greet to index jsx

--- src/dforms_assets/src/index.jsx
+++ src/dforms_assets/src/index.jsx
@@ -2,6 +2,28 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { dforms as canister } from "../../declarations/dforms";
 
+import { dforms } from "../../declarations/dforms";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await dforms.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
+
+
+
 class PhoneBook extends React.Component {
 
   constructor(props) {
