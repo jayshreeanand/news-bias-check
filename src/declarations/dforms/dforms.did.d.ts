d71bdecdf0a772c74248b31bcedcfd95ecbde9ff
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-06-21 05:23:31 +0530

    Added react bootstrap, udpated build

--- src/declarations/dforms/dforms.did.d.ts
+++ src/declarations/dforms/dforms.did.d.ts
@@ -3,9 +3,17 @@ import type { ActorMethod } from '@dfinity/agent';
 
 export interface Entry { 'desc' : string, 'phone' : Phone }
 export type Name = string;
+export interface NewsSource {
+  'domain' : string,
+  'source' : string,
+  'bias' : string,
+  'accuracy' : string,
+}
 export type Phone = string;
 export interface _SERVICE {
   'greet' : ActorMethod<[string], string>,
-  'insert' : ActorMethod<[Name, Entry], undefined>,
-  'lookup' : ActorMethod<[Name], [] | [Entry]>,
+  'insert' : ActorMethod<[string, string, string, string], undefined>,
+  'insertOld' : ActorMethod<[Name, Entry], undefined>,
+  'lookup' : ActorMethod<[string], [] | [NewsSource]>,
+  'lookupOld' : ActorMethod<[Name], [] | [Entry]>,
 }
--- src/declarations/dforms/dforms.did.js
+++ src/declarations/dforms/dforms.did.js
@@ -2,10 +2,18 @@ export const idlFactory = ({ IDL }) => {
   const Name = IDL.Text;
   const Phone = IDL.Text;
   const Entry = IDL.Record({ 'desc' : IDL.Text, 'phone' : Phone });
+  const NewsSource = IDL.Record({
+    'domain' : IDL.Text,
+    'source' : IDL.Text,
+    'bias' : IDL.Text,
+    'accuracy' : IDL.Text,
+  });
   return IDL.Service({
     'greet' : IDL.Func([IDL.Text], [IDL.Text], []),
-    'insert' : IDL.Func([Name, Entry], [], []),
-    'lookup' : IDL.Func([Name], [IDL.Opt(Entry)], ['query']),
+    'insert' : IDL.Func([IDL.Text, IDL.Text, IDL.Text, IDL.Text], [], []),
+    'insertOld' : IDL.Func([Name, Entry], [], []),
+    'lookup' : IDL.Func([IDL.Text], [IDL.Opt(NewsSource)], ['query']),
+    'lookupOld' : IDL.Func([Name], [IDL.Opt(Entry)], ['query']),
   });
 };
 export const init = ({ IDL }) => { return []; };
