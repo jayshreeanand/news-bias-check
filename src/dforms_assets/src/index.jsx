b899b026be3ff379f03fb2d45327eaf784ff7cff
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-06-20 16:40:15 +0530

    UI changes

--- src/dforms_assets/src/index.jsx
+++ src/dforms_assets/src/index.jsx
@@ -2,25 +2,25 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { dforms as canister } from "../../declarations/dforms";
 
-// import { dforms } from "../../declarations/dforms";
+import { dforms } from "../../declarations/dforms";
 
-// document.querySelector("form").addEventListener("submit", async (e) => {
-//   e.preventDefault();
-//   const button = e.target.querySelector("button");
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
 
-//   const name = document.getElementById("name").value.toString();
+  const name = document.getElementById("name").value.toString();
 
-//   button.setAttribute("disabled", true);
+  button.setAttribute("disabled", true);
 
-//   // Interact with foo actor, calling the greet method
-//   const greeting = await dforms.greet(name);
+  // Interact with foo actor, calling the greet method
+  const greeting = await dforms.greet(name);
 
-//   button.removeAttribute("disabled");
+  button.removeAttribute("disabled");
 
-//   document.getElementById("greeting").innerText = greeting;
+  document.getElementById("greeting").innerText = greeting;
 
-//   return false;
-// });
+  return false;
+});
 
 
 
